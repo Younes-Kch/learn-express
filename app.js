6290db556e5b3ec47ba4c72791bfb915926d801e
Younes <116138482+Younes-Kch@users.noreply.github.com>
2023-07-01 15:59:49 +0200

    setup express/nodemon complete

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("requete reçu");
+  next()
+});
+app.use((req, res, next) => {
+  res.json({ message: "Votre requete a bien été reçue" });
+});
+
+module.exports = app;
