db1b90e4361707eddf31551aa2e39d63300f3c12
Younes <116138482+Younes-Kch@users.noreply.github.com>
2023-07-22 09:55:58 +0200

    security(user): add jwttoken package and implement in login

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
@@ -29,7 +31,14 @@ exports.login = (req, res, next) => {
           .compare(req.body.password, user.password)
           .then((valid) => {
             if (valid) {
-              res.status(200).json({ userId: user._id, token: "TOKEN" });
+              res
+                .status(200)
+                .json({
+                  userId: user._id,
+                  token: jwt.sign({ userId: user._id }, "RANDOM-TOKEN-SECRET", {
+                    expiresIn: "24h",
+                  }),
+                });
             } else {
               res
                 .status(401)
